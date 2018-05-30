f802b422ff5cd639245a3a202cbd4287460db488
Matteo Tressi <m.tressi@gmail.com>
2018-05-30 17:38:19 +0200

    unisce la logica della pagina admin in un unico file js

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -5,12 +5,15 @@ const approvedListRef = 'approvate';
 const membersList = document.getElementById('membersList');
 const membersListRef = 'soci';
 
+var selectedCards = [];
+
 const navButtons = document.getElementsByClassName('pg__tab');
 
 for (i = 0; i < navButtons.length; ++i) {
   navButtons[i].addEventListener("click", function() {
     console.log("you clicked button controler " + this.id + "!");
-    readList(this.id)
+    readList(this.id);
+    selectedCards = [];  //empty select cards list
   });
 }
 
@@ -26,8 +29,9 @@ function readList(navButtonId) {
       for (i = 0; i < listArray.length; ++i) {
         listArray[i].style.display = '';
       }
-      // SELECT CARDS FOR THIS LIST
-      var userCard = document.getElementsByClassName('pg__tab-' + thisList);
+      //SELECT CARDS FOR THIS LIST
+      var userCard = document.getElementsByClassName('pg__tab-' + thisList.id);
+      console.log(thisList.id);
       //CLEAN PAGE FROM PREVIOUS VISITS
       while (userCard.length > 0) {
         userCard[0].parentNode.removeChild(userCard[0]);
@@ -50,7 +54,7 @@ function readList(navButtonId) {
 
             createCard(userName, userSurname, userEmail, key, thisList);
           });
-          // runForLoop(requestCard);
+          runForLoop(userCard);
         });
     }
   }
@@ -59,7 +63,7 @@ function readList(navButtonId) {
 //create and append in page a card for every request
 function createCard(userName, userSurname, userEmail, key, thisList) {
   var newCard = document.createElement('div');
-  newCard.className = "pg__tab-" + thisList;
+  newCard.className = "pg__tab-" + thisList.id;
   newCard.id = key;
 
   var paraName = document.createElement('p');
@@ -74,6 +78,97 @@ function createCard(userName, userSurname, userEmail, key, thisList) {
   newCard.appendChild(paraSurname);
   newCard.appendChild(paraEmail);
 
-  pendingList.appendChild(newCard);
-  pendingList.style.display = 'block';
+  thisList.appendChild(newCard);
+}
+
+// SELECT AND DESELECT CARDS //
+
+//add select or deselct card event on click
+function listenClickEvent(cardId) {
+  var thisCard = document.getElementById(cardId);
+
+  thisCard.addEventListener('click', function() {
+    selectDeselectCard(cardId)
+  });
+}
+
+//check if card is selected or not
+function selectDeselectCard(cardId) {
+  var isCardSelected = selectedCards.indexOf(cardId);
+
+  isCardSelected > -1 ? deselectCard(cardId) : selectCard(cardId);
+}
+
+//select card and add it to array
+function selectCard(cardId) {
+  var thisCard = document.getElementById(cardId);
+
+  thisCard.style.borderColor = 'red';
+  selectedCards.push(cardId);
+  console.log(selectedCards);
+}
+
+//deselect card and remove it from array
+function deselectCard(cardId) {
+  var thisCard = document.getElementById(cardId);
+  var thisCardIndex = selectedCards.indexOf(cardId);
+
+  thisCard.style.borderColor = 'transparent';
+  selectedCards.splice(thisCardIndex, 1);
+  console.log(selectedCards);
+}
+//add event listener to all cards in list
+function runForLoop(cardsList) {
+  for (var i = 0; i < cardsList.length; i++) {
+    var elementId = cardsList[i].id;
+    listenClickEvent(elementId);
+  }
+}
+
+// MOVE CARDS TO SELECTED FOLDER //
+
+function moveCard(key, moveElementsTo) { //move selected cards to desired folder
+  moveElementsTo === 'approveSelectedRequests' ? moveElementsFromTo(pendingListRef, approvedListRef) : '';
+  moveElementsTo === 'confirmNewMembers' ? moveElementsFromTo(approvedListRef, membersListRef) : '';
+  // moveElementsTo === 'deleteMember' ? console.log('Move ' + key + ' to ' + moveElementsTo) : '';
+
+  function moveElementsFromTo(moveFrom, moveTo) {
+    var sourceList = firebase.database().ref(moveFrom).child(key);
+    var destinationList = firebase.database().ref(moveTo).child(key);
+
+    sourceList.on("value", function(snapshot) {
+
+      destinationList.set(snapshot.val(), deleteMovedCard(key, moveFrom)); // write card to new location and run delete function on callback
+
+    }, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  }
+}
+
+function moveSelectedCards(buttonId) {
+  for (var i = 0; i < selectedCards.length; i++) {
+    var elementId = selectedCards[i];
+    moveCard(elementId, buttonId);
+  }
+}
+
+function deleteMovedCard(key, deleteFrom) {
+  firebase.database().ref(deleteFrom).child(key).remove().then(function() {
+    refreshModifiedList(deleteFrom);
+  }, function(error) {
+    console.log(error);
+  });
+}
+
+// update list in page to display only not moved cards
+function refreshModifiedList(deleteFrom) {
+  //HIDE ALL LISTS
+  let listArray = document.getElementsByClassName('pg__list');
+  for (i = 0; i < listArray.length; ++i) {
+    listArray[i].style.display = '';
+  }
+
+  deleteFrom === pendingListRef ? readList('readPendingButton') : '';
+  deleteFrom === approvedListRef ? readList('readApprovedButton') : '';
 }
